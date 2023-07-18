b939a85749c19a0fa85d7522982296c219910d1f
Chris CR <chris_49git@outlook.com>
2023-07-18 13:20:46 -0600

    Inicializando el proyecto de React

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Css/App.css";
+import { Prueba } from "./Components/Prueba";
+
+function App() {
+  return (
+    <div className="App">
+      <Prueba />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Prueba.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Prueba = () => {
+  return (
+    <div><h1>Este es un componente de prueba</h1></div>
+  )
+}
+ 

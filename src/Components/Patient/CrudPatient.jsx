5587073186234a5dd9cfe158fc693861c0b673cc
Chris CR <chris_49git@outlook.com>
2023-08-01 14:03:04 -0600

    This code is only prepared to make the connection to the backend

--- src/Components/Patient/CrudPatient.jsx
+++ src/Components/Patient/CrudPatient.jsx
@@ -6,6 +6,8 @@ export const CrudPatient = () => {
   const { getAllPatientsData, dataTest } = useContext(GetTheAppContext);
   return (
     <div>
+      {/* TODO: This data "dataTest" is for testing purposes, it should be removed when the app is ready. */}
+      {/* TODO: "getAllPatientsData" is the variable that contains the data from the API. */}
       <TablePatient dataTable={dataTest || []} />
     </div>
   );

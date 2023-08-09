fafaecc8b1ee196ae39b0b43bf49c2db2ae6bfc7
Chris CR <chris_49git@outlook.com>
2023-08-09 15:00:49 -0600

    Update

---
+++ src/Context/History.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+const patientURL = "http://localhost:8081/iwaid/patients/";
+
+export const getAllPatientDataFunction = async (setGetAllPatients) => {
+  try {
+    const response = await axios.get(patientURL);
+    setGetAllPatients(response.data);
+  } catch (error) {
+    console.error("Error al enviar la solicitud:", error);
+  }
+};
+
+// export const createPatientFunction = async (arrayData) => {
+//   try {
+//     const response = await axios.post(patientURL, arrayData);
+//     return response;
+//   } catch (error) {
+//     console.error("Error al enviar la solicitud:", error);
+//     return error;
+//   }
+// };
+
+// export const updatePatientFunction = async (arrayData, idPatient) => {
+//   const urlUpdate = `${patientURL}${idPatient}`;
+//   try {
+//     const response = await axios.patch(urlUpdate, arrayData);
+//     return response;
+//   } catch (error) {
+//     console.error("Error al enviar la solicitud:", error);
+//     return error;
+//   }
+// };
+
+// export const deletePatientFunction = async (idPatient) => {
+//   try {
+//     const urlDelete = `${patientURL}${idPatient}`;
+//     const response = await axios.delete(urlDelete);
+//     return response;
+//   } catch (error) {
+//     console.error("Error al enviar la solicitud:", error);
+//     return error;
+//   }
+// };

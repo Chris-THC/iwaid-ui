28a27204f6e24a02e9313723eedd0274560c607c
Chris <104177004+Chris-THC@users.noreply.github.com>
2023-08-10 17:25:59 -0600

    IWAID-34 Componente genérico para eliminación (#14)
    * The modal to delete is ready
    
    * The modal to delete is ready
    
    * Update
    
    * component delete all is ready

--- src/Components/Date/table.jsx
+++ src/Components/Date/table.jsx
@@ -1,21 +1,21 @@
-import React, {useState, useContext} from 'react';
-import '../../Css/CssTable.css';
+import React, { useState, useContext } from "react";
+import "../../Css/CssTable.css";
 import { GetTheAppContext } from "../../Context/AppContext";
-import { ModalMedicine } from './modal';
-import { ModalDelete } from './modalDelete';
-import { BsPersonFillAdd, BsPencilFill  } from "react-icons/bs";
-import { MdDeleteForever } from 'react-icons/md';
+import { ModalMedicine } from "./modal";
+import { ModalDelete } from "../../ModalDelete/ModalDelete";
+import { BsPersonFillAdd, BsPencilFill } from "react-icons/bs";
+import { MdDeleteForever } from "react-icons/md";
 import { LuFilterX } from "react-icons/lu";
 import { Button } from "react-bootstrap";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import Tooltip from "react-bootstrap/Tooltip";
+import { statusDeleted } from "./HTTPStatus";
 
 export function TablaGeneric({ title, data }) {
-
   const [showModalDelete, setShowModalDelete] = useState(false);
   const [searchNameDoctor, setSearchNameDoctor] = useState("");
   const [searchNamePatient, setSearchNamePatient] = useState("");
-  
+
   const {
     handleShowModal,
     handleCloseModal,
@@ -24,8 +24,13 @@ export function TablaGeneric({ title, data }) {
     setActionButtonModal,
     setIdDate,
     setDataUserDate,
+    idDate,
+    deleteDateFunction,
+    getAllDateDataFunction,
+    setAllDataDate,
+    setTextAlert,
+    handleShowFloatAlter,
   } = useContext(GetTheAppContext);
-
 
   const handleClear = () => {
     setSearchNameDoctor("");
@@ -34,7 +39,6 @@ export function TablaGeneric({ title, data }) {
 
   const handleShowModalDelete = () => {
     setShowModalDelete(true);
-    
   };
 
   const handleCloseModalDelete = () => {
@@ -56,10 +60,25 @@ export function TablaGeneric({ title, data }) {
     SEVEN_PM: "7:00-7:59 PM",
     EIGHT_PM: "8:00-8:59 PM",
   };
-  
+
   const formatDate = (originalDate) => {
-   let piecesDate=originalDate.split("-");
-   return piecesDate[2]+"/"+piecesDate[1]+"/"+piecesDate[0]
+    let piecesDate = originalDate.split("-");
+    return piecesDate[2] + "/" + piecesDate[1] + "/" + piecesDate[0];
+  };
+
+  const funtionToDeleted = async () => {
+    setActionButtonModal("Eliminar");
+    const responseModalDelete = await deleteDateFunction(idDate);
+    if (responseModalDelete.status === statusDeleted) {
+      await getAllDateDataFunction(setAllDataDate);
+      setTextAlert(`Se eliminó la cita`);
+      handleCloseModalDelete();
+      handleShowFloatAlter();
+    } else {
+      handleCloseModalDelete();
+      setTextAlert(`Error al eliminar la cita`);
+      handleShowFloatAlter();
+    }
   };
 
   return (
@@ -91,62 +110,61 @@ export function TablaGeneric({ title, data }) {
         </div>
 
         <div className="card-header col-md-12">
-  <div className="card-body table-responsive">
-    <div className="container">
-      <div className="row mb-3">
-        <div className="col-md-4">
-          <h4>Buscar</h4>
-        </div>
-      </div>
-      <div className="row mb-4">
-        <div className="col-md-4">
-          <label>Nombre del doctor</label>
-          <input
-            autoComplete="off"
-            type="text"
-            className="form-control"
-            value={searchNameDoctor}
-            onChange={(e) => {
-              setSearchNameDoctor(e.target.value);
-            }}
-            placeholder="Buscar por nombre..."
-            pattern="^[A-Za-z\s]+$"
-          />
-        </div>
-        <div className="col-md-4">
-          <label>Nombre del paciente</label>
-          <input
-            autoComplete="off"
-            type="text"
-            className="form-control"
-            value={searchNamePatient}
-            onChange={(e) => {
-              setSearchNamePatient(e.target.value);
-            }}
-            placeholder="Buscar por nombre..."
-            pattern="^[A-Za-z\s]+$"
-          />
-        </div>
+          <div className="card-body table-responsive">
+            <div className="container">
+              <div className="row mb-3">
+                <div className="col-md-4">
+                  <h4>Buscar</h4>
+                </div>
+              </div>
+              <div className="row mb-4">
+                <div className="col-md-4">
+                  <label>Nombre del doctor</label>
+                  <input
+                    autoComplete="off"
+                    type="text"
+                    className="form-control"
+                    value={searchNameDoctor}
+                    onChange={(e) => {
+                      setSearchNameDoctor(e.target.value);
+                    }}
+                    placeholder="Buscar por nombre..."
+                    pattern="^[A-Za-z\s]+$"
+                  />
+                </div>
+                <div className="col-md-4">
+                  <label>Nombre del paciente</label>
+                  <input
+                    autoComplete="off"
+                    type="text"
+                    className="form-control"
+                    value={searchNamePatient}
+                    onChange={(e) => {
+                      setSearchNamePatient(e.target.value);
+                    }}
+                    placeholder="Buscar por nombre..."
+                    pattern="^[A-Za-z\s]+$"
+                  />
+                </div>
 
-        <div className="col-md-4 d-flex align-items-center mt-4">
-  <OverlayTrigger
-    placement="top"
-    overlay={<Tooltip id="tooltip-clear">Limpiar</Tooltip>}
-  >
-    <button
-      id="iconoClear"
-      className="btn btn-primary"
-      type="button"
-      onClick={handleClear}
-    >
-      <LuFilterX color="white" />
-    </button>
-  </OverlayTrigger>
-</div>
+                <div className="col-md-4 d-flex align-items-center mt-4">
+                  <OverlayTrigger
+                    placement="top"
+                    overlay={<Tooltip id="tooltip-clear">Limpiar</Tooltip>}
+                  >
+                    <button
+                      id="iconoClear"
+                      className="btn btn-primary"
+                      type="button"
+                      onClick={handleClear}
+                    >
+                      <LuFilterX color="white" />
+                    </button>
+                  </OverlayTrigger>
+                </div>
+              </div>
+            </div>
 
-      </div>
-    </div>
- 
             <table className="table table-bordered custom-table text-center">
               <thead>
                 <tr>
@@ -160,82 +178,88 @@ export function TablaGeneric({ title, data }) {
               </thead>
               <tbody>
                 {data
-                .filter((field)=>{
-                  const patienNameMatches = field.patientDTO.name
-                  .toLowerCase()
-                  .includes(searchNamePatient.toLowerCase());
-                  
-                  const doctorNameMatches = field.doctorDTO.name
-                  .toLowerCase()
-                  .includes(searchNameDoctor.toLowerCase());
+                  .filter((field) => {
+                    const patienNameMatches = field.patientDTO.name
+                      .toLowerCase()
+                      .includes(searchNamePatient.toLowerCase());
 
-                  return patienNameMatches&&doctorNameMatches;
-                })
-              .map((field)=>(
-                <tr key={field.id}>
-                 <td>{field.patientDTO.name}</td>
-                  <td>{field.doctorDTO.name}</td>
-                   <td>{formatDate(field.date)}</td>
-                  <td>{hourMappings[field.hour]}</td>
-                  <td>{field.notes}</td>
-                    <td className="Buttons">
-                      <OverlayTrigger
-                        placement="top"
-                        overlay={<Tooltip id="tooltip-clear">Editar</Tooltip>}
-                      >
-                        <Button
-                          size={13}
-                          id="btnTables"
-                          className="ms-2 me-2 mb-2 mt-2"
-                          variant="primary"
-                          onClick={() => {
-                            handleShowModal();
-                            setIdDate(field.id);
-                            setGetDataFromTable(field);
-                            setActionButtonModal("Editar");
-                          }}
-                        >
-                          <BsPencilFill className="btn-icon-lg" />
-                        </Button>
-                      </OverlayTrigger>
+                    const doctorNameMatches = field.doctorDTO.name
+                      .toLowerCase()
+                      .includes(searchNameDoctor.toLowerCase());
 
-                      <OverlayTrigger
-                        placement="top"
-                        overlay={<Tooltip id="tooltip-clear">Eliminar</Tooltip>}
-                      >
-                        <Button
-                          size={16}
-                          id="btnTables"
-                          className="ms-2 me-2 mb-2 mt-2 d-inline "
-                          variant="danger"
-                          onClick={() => {
-                            setIdDate(field.id);
-                            setDataUserDate(field);
-                            handleShowModalDelete();
-                          }}
+                    return patienNameMatches && doctorNameMatches;
+                  })
+                  .map((field) => (
+                    <tr key={field.id}>
+                      <td>{field.patientDTO.name}</td>
+                      <td>{field.doctorDTO.name}</td>
+                      <td>{formatDate(field.date)}</td>
+                      <td>{hourMappings[field.hour]}</td>
+                      <td>{field.notes}</td>
+                      <td className="Buttons">
+                        <OverlayTrigger
+                          placement="top"
+                          overlay={<Tooltip id="tooltip-clear">Editar</Tooltip>}
                         >
-                          <MdDeleteForever
+                          <Button
                             size={13}
+                            id="btnTables"
+                            className="ms-2 me-2 mb-2 mt-2"
+                            variant="primary"
                             onClick={() => {
-                              setDataUserDate(field);
+                              handleShowModal();
+                              setIdDate(field.id);
+                              setGetDataFromTable(field);
+                              setActionButtonModal("Editar");
                             }}
-                            id="btnDelete"
-                            className="btn-icon-lg"
-                          />
-                        </Button>
-                      </OverlayTrigger>
-                    </td>
-                  </tr>
-                ))}
+                          >
+                            <BsPencilFill className="btn-icon-lg" />
+                          </Button>
+                        </OverlayTrigger>
 
+                        <OverlayTrigger
+                          placement="top"
+                          overlay={
+                            <Tooltip id="tooltip-clear">Eliminar</Tooltip>
+                          }
+                        >
+                          <Button
+                            size={16}
+                            id="btnTables"
+                            className="ms-2 me-2 mb-2 mt-2 d-inline "
+                            variant="danger"
+                            onClick={() => {
+                              setIdDate(field.id);
+                              setDataUserDate(field);
+                              handleShowModalDelete();
+                            }}
+                          >
+                            <MdDeleteForever
+                              size={13}
+                              onClick={() => {
+                                setDataUserDate(field);
+                              }}
+                              id="btnDelete"
+                              className="btn-icon-lg"
+                            />
+                          </Button>
+                        </OverlayTrigger>
+                      </td>
+                    </tr>
+                  ))}
               </tbody>
             </table>
           </div>
+
           <ModalMedicine show={showModal} handleClose={handleCloseModal} />
-          <ModalDelete show={showModalDelete} handleClose={handleCloseModalDelete}  />
+          <ModalDelete
+            show={showModalDelete}
+            handleClose={handleCloseModalDelete}
+            funtionToDeleted={funtionToDeleted}
+            messageToDelete={"¿Está seguro de que desea eliminar esta cita?"}
+          />
         </div>
       </div>
     </div>
-  
   );
 }

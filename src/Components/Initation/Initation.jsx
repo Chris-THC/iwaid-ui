6b29cf96af47d1da1eedbd715a333256103bc16b
Mau03gm <gatojs0109@outlook.com>
2023-08-10 18:32:11 -0600

    logo size

--- src/Components/Initation/Initation.jsx
+++ src/Components/Initation/Initation.jsx
@@ -7,47 +7,20 @@ import camilla from "../Images/c.jpg";
 import doc from "../Images/doctor2.jpg"
 import LogoFooter from"../Images/IWALogoFooter.jpg";
 import { Carousel } from 'react-bootstrap';
-import { FaInstagram } from 'react-icons/fa';
-import { FaWhatsapp } from 'react-icons/fa';
-import { FaFacebook } from 'react-icons/fa';
+import { FaInstagram,FaWhatsapp, FaFacebook  } from 'react-icons/fa';
 
 export function IMG(){
     return(
         <div>
-        <img className="imgLogo"  src={logoImage} alt="Imagen Logo" />
+        <img className="imgLogo"  src={logoImage} alt="Imagen Logo"  width="150" height="50" />
         </div>
     );
 }
 
-export function Initation(){
-    return(
-        <div class="">
-          
-            <img className="imgInicio" src={consultorio} alt="" />
-
-        </div> 
-    );
-}
-
 export const CarouselComponent = () => {
-  const settings = {
-    infinite: true, 
-    speed: 500, 
-    slidesToShow: 1, 
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000 
-  };
   return (
     <>
         <Carousel>
-      <Carousel.Item>
-        <img className="d-block w-100 h-100" src={consultorio} alt="img-consult" />
-        <Carousel.Caption>
-          <h5>Salud Brillante</h5>
-          <p>Nuestro compromiso es tu bienestar. Cuidamos de ti para que vivas cada día al máximo.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
       <Carousel.Item>
         <img className="d-block w-100 h-100" src={camilla} alt="img-strecher" />
         <Carousel.Caption>
@@ -69,6 +42,13 @@ export const CarouselComponent = () => {
           <p>En nuestros consultorios, trazamos juntos la ruta hacia una vida más saludable. Tu salud, nuestra prioridad.</p>
         </Carousel.Caption>
       </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100 h-100" src={consultorio} alt="img-consult" />
+        <Carousel.Caption>
+          <h5>Salud Brillante</h5>
+          <p>Nuestro compromiso es tu bienestar. Cuidamos de ti para que vivas cada día al máximo.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
     </Carousel>
     </>
   );
@@ -78,7 +58,6 @@ export const Footer =()=>{
   return(
     <div class="card-group ">
   <div class="card " style={{ backgroundColor: "#15ACF7" }}>
-
     <div class="card-body ">
     <img className="imgLogo"  src={LogoFooter} alt="Imagen Logo" />
       <h5 class="card-title text-light">Valores que nos Definen</h5>
@@ -95,7 +74,6 @@ Cuidado de Enfermedades Crónicas: Manejamos con experiencia condiciones médica
 Vacunación y Prevención: Protegemos tu salud a través de programas de vacunación personalizados.
 Atención en Urgencias Menores: Estamos aquí para brindarte atención en situaciones que requieren atención médica inmediata.
 Asesoría Nutricional: Guiamos hacia hábitos alimenticios que fomentan un estilo de vida saludable.</p>
-     
     </div>
   </div>
   <div class="card  "style={{ backgroundColor: "#15ACF7" }}>

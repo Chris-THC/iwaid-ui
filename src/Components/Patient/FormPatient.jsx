226c46a835eb0f171506c1a811c16154198e6e48
Chris CR <chris_49git@outlook.com>
2023-08-18 21:16:20 -0600

    Develop branch was restored

--- src/Components/Patient/FormPatient.jsx
+++ src/Components/Patient/FormPatient.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react";
 import { Modal, Button } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { GetTheAppContext } from "../../Context/AppContext";
-import { statusCreated, statusOk  } from "../HttpStatus/HTTPStatusCode";
+import { statusCreated } from "../HttpStatus/HTTPStatusCode";
 export const FormPatient = ({ isGetData = {} }) => {
   const currentDate = new Date().toISOString().split("T")[0];
 
@@ -46,7 +46,7 @@ export const FormPatient = ({ isGetData = {} }) => {
         patientId
       );
 
-      if (updatePatientResponse.status === statusCreated ) {
+      if (updatePatientResponse.status === statusCreated) {
         setTextAlert(`Paciente ${data.name} actualizado exitosamente`);
         await getAllPatientDataFunction(setGetAllPatientsData);
         handleShowFloatAlter();
@@ -257,9 +257,14 @@ export const FormPatient = ({ isGetData = {} }) => {
 
         <div>
           <Modal.Footer>
-          <button type="button" class="btn btn btn-light  btn-outline-danger"
-          onClick={handleCloseModal}
-          data-bs-dismiss="modal">Cancelar</button>
+            <button
+              type="button"
+              class="btn btn btn-light  btn-outline-danger"
+              onClick={handleCloseModal}
+              data-bs-dismiss="modal"
+            >
+              Cancelar
+            </button>
             <Button
               type="submit"
               onClick={() => {

73c52befb694e6f669d5b15c034cd76807664cf7
Chris CR <chris_49git@outlook.com>
2023-08-30 21:53:16 -0600

    Well for some reason in npx create-react-app the dotenv library doesn't work, so I opteded to created a global constant

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import App from "./App";
 import { AppContext } from "./Context/AppContext";
 import { BrowserRouter } from "react-router-dom";
-
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 

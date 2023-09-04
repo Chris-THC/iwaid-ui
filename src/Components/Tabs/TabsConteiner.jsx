922a00b1f7cdea8ab0c49cdc3635b8a2443df588
Chris CR <chris_49git@outlook.com>
2023-09-04 09:43:13 -0600

    The routes is update and it was restructured

--- src/Components/Tabs/TabsConteiner.jsx
+++ src/Components/Tabs/TabsConteiner.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-// import { PersonalDates } from "./pages/PersonalDates";
-// import { PersonalHistory } from "./pages/PersonalHistory";
-// import { PersonalPrescriptions } from "./pages/PersonalPrescriptions";
-// import { Personalformation } from "./pages/Personalformation";
 import { Tabs } from "./Tabs";
 
 export const TabsConteiner = () => {
@@ -11,16 +7,6 @@ export const TabsConteiner = () => {
     <div>
       <Tabs />
       <Outlet />
-      {/* <Routes>
-        <Route path="/" element={<Personalformation />} />
-        <Route path="/medical/dates" element={<PersonalDates />} />
-        <Route
-          path="/medical/prescriptions"
-          element={<PersonalPrescriptions />}
-        />
-        <Route path="/medical/history" element={<PersonalHistory />} />
-        <Route path="/*" element={<div>Error 404 Not Found Page</div>} />
-      </Routes> */}
     </div>
   );
 };
